2d65dae0aab37b6d229801646b1f6db7e56875f4
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-22 14:27:03 +0000

    short

--- resto_web/src/routes/Routers.jsx
+++ resto_web/src/routes/Routers.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
+import MenuResto from '../pages/MenuResto'
+import Resume from '../pages/Resume'
 
 
 const Routers = () => {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menuAllFood" element={<MenuResto />} />
+        <Route path="/commandeByTable" element={<Resume />} />
     </Routes>
   )
 }
