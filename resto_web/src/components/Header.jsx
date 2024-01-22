72285317ea2238415b09102c6156bcc972ce3787
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-22 15:22:25 +0000

    short

--- resto_web/src/components/Header.jsx
+++ resto_web/src/components/Header.jsx
@@ -2,13 +2,27 @@ import React from 'react'
 import Link from 'react-scroll'
 
 const Header = () => {
-  const content = 
-    <>
-    
-    </>
+  const navLinks = [
+    {
+      path:'/home',
+      display: 'Home'
+    },
+    {
+      path:'/doctors',
+      display: 'Find a Doctor'
+    },
+    {
+      path:'/services',
+      display: 'Services'
+    },
+    {
+      path:'/contact',
+      display: 'Contact'
+    },
+  ]
 
   return (
-    <div className="bg-slate-900">
+    <div>
       
     </div>
   )
