0db16075b1473214aad4dc4349404a79eba35400
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-27 20:52:15 +0000

    short

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-4xl border">
+        <main className="max-w-4xl mx-auto border">
           {children}
         </main>
       </body>
