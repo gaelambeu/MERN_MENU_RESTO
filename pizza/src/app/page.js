58944a33ab233edda10011c906c8aaa7a95ca862
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-27 22:14:46 +0000

    short

--- pizza/src/app/page.js
+++ pizza/src/app/page.js
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>test</div>
+    <>
+      <header>
+        <a href=""></a>
+      </header>
+    </>
   );
 }
