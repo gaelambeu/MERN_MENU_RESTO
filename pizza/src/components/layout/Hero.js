a6d59b33de652b0dc00572bd34c0cf012adc037c
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-28 14:24:56 +0000

    short

--- pizza/src/components/layout/Hero.js
+++ pizza/src/components/layout/Hero.js
@@ -2,12 +2,14 @@ import Image from "next/image"
 
 export default function Hero() {
     return(
-        <section>
-            <h1>Everything is better with a Pizza</h1>
-            <p>
-                Pizza is the missing piece that makes every day
-                complete, a simple yet delicious joy in life
-            </p>
+        <section className="grid grid-cols-2">
+            <div>
+                <h1>Everything is better with a Pizza</h1>
+                <p>
+                    Pizza is the missing piece that makes every day
+                    complete, a simple yet delicious joy in life
+                </p>
+            </div>
             <div className="w-16 h-16 relative">
                 <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'} />
             </div>
