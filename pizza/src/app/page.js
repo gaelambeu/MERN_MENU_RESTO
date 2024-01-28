5d9e9a628a2be7c49ff5b495b65af0f35491bc45
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-28 13:51:15 +0000

    short

--- pizza/src/app/page.js
+++ pizza/src/app/page.js
@@ -7,12 +7,12 @@ export default function Home() {
         <Link className="text-primary font-semibold text-2xl" href="">
           ST PIZZA
         </Link>
-        <nav className="flex gap-4">
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
           <Link href={''}>Home</Link>
           <Link href={''}>Menu</Link>
           <Link href={''}>About</Link>
           <Link href={''}>Contact</Link>
-          <Link href={''}>Login</Link>
+          <Link href={''} className="bg-primary rounded-full text-white px-8 py-2">Login</Link>
         </nav>
       </header>
     </>
