32bd59c788332028fc8a935b0fcfbaa8e168a3f9
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-08-21 20:31:22 +0300

    short

--- floris_hotel/src/app/page.js
+++ floris_hotel/src/app/page.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>test</div>
+    <>
+      <header>
+        <Link className="text-green-500 font-semibold" href="">FLORIS HÔTEL</Link>
+        <nav>
+          <Link href={''}>Home</Link>
+          <Link href={''}>Menu</Link>
+          <Link href={''}>About</Link>
+          <Link href={''}>Contact</Link>
+          <Link href={''}>Login</Link>
+        </nav>
+      </header>
+    </>
   )
 }
