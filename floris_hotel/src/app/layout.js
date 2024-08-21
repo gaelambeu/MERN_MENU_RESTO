8f03c0dfe3dfed9849cfcfed0c750861d7880380
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-08-21 20:03:57 +0300

    installing project

---
+++ floris_hotel/src/app/layout.js
@@ -0,0 +1,17 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
+
+export const metadata = {
+  title: "E-Menu Restaurant",
+  description: "Floris Hôtel Menu restaurant ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
